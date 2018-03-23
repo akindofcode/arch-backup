a5e85ddebef236e86347433e34622096e32b6bf5
akindofcode <akindofcode@gmail.com>
2018-03-23 12:31:55 +0100

    Added 002-fast-mirrors.sh

---
+++ 002-fast-mirrors.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+sudo pacman -S --noconfirm --needed reflector
+
+sudo reflector -l 100 -f 50 --sort rate --threads 5 --verbose --save /tmp/mirrorlist.new && rankmirrors -n 0 /tmp/mirrorlist.new > /tmp/mirrorlist && sudo cp /tmp/mirrorlist /etc/pacman.d
+
+cat /etc/pacman.d/mirrorlist
+
+sudo pacman -Syyu --noconfirm
