22f9a9f3015db1f9d238a9af7cd19c92c6ab4a01
akindofcode <akindofcode@users.noreply.github.com>
2018-03-23 17:07:47 +0100

    added utils

--- 008-conf-openbox.sh
+++ 008-conf-openbox.sh
@@ -10,6 +10,8 @@ sudo pacman -S --noconfirm --needed neofetch
 
 sudo pacman -S --noconfirm --needed oblogout
 
+sudo pacman -S --noconfirm --needed tint2 dmenu feh nitrogen rofi xfce4-appfinder gmrun numlockx xfce4-notifyd volumeicon
+
 packer -S --noconfirm --noedit obmenu3
 
 packer -S --noconfirm --noedit obmenu-generator
@@ -20,5 +22,13 @@ packer -S --noconfirm --noedit obkey
 
 packer -S --noconfirm --noedit i3lock-color-git
 
+packer -S --noconfirm --noedit polybar-git
+
 # copy default openbox configuration to ~/.config
 cp -R /etc/xdg/openbox ~/.config
+
+echo "tint2 &" >> ~/.config/openbox/autostart
+echo "nitrogen --restore & #restore the background chosen with nitrogen" >> ~/.config/openbox/autostart
+echo "compton -b -c --config ~/.config/compton/compton.conf &" >> ~/.config/openbox/autostart
+echo "volumeicon &" >> ~/.config/openbox/autostart
+echo "numlockx &" >> ~/.config/openbox/autostart
