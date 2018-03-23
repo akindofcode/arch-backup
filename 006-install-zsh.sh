925e726e4c6383a4f567342b9069ed353f6e4d15
akindofcode <akindofcode@users.noreply.github.com>
2018-03-23 17:39:22 +0100

    added nerd-fonts-complete

--- 006-install-zsh.sh
+++ 006-install-zsh.sh
@@ -9,12 +9,14 @@ sudo pacman -S --noconfirm --needed zsh-syntax-highlighting
 # install oh-my-zsh from github
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
-sudo pacman -S --noconfirm --needed powerline-fonts
+sudo pacman -S --noconfirm --needed powerline-fonts 
+
+packer -S --noconfirm --noedit nerd-fonts-complete
 
 # To hange the theme, go to the hidden file ~/.zshrc
 # change this to ZSH_THEME="agnoster"
 
-packer -S --noconfirm --noedit command-not-found
+#packer -S --noconfirm --noedit command-not-found
 
 sudo chsh $USER -s /bin/zsh
 
