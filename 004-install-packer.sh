65c0b38052480fa0802672c33bfa6ee72df6f8b2
akindofcode <akindofcode@gmail.com>
2018-03-23 12:32:37 +0100

    Added 004-install-packer.sh

---
+++ 004-install-packer.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+sudo pacman -S --noconfirm --needed grep sed bash curl pacman jshon expac
+
+[ -d /tmp/packer ] && rm -rf /tmp/packer
+
+mkdir /tmp/packer
+
+wget https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=packer
+
+mv PKGBUILD\?h\=packer /tmp/packer/PKGBUILD
+
+cd /tmp/packer
+
+makepkg -i /tmp/packer --noconfirm
+
+[ -d /tmp/packer ] && rm -rf /tmp/packer
+
+# Just checking if installation was successful
+if pacman -Qi packer &> /dev/null; then
+
+echo "### packer has been installed ####"
+
+else
+
+echo "!!! packer has NOT been installed !!!"
+
+fi
