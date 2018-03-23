08ac97d426c7cf5ad47b30b9cddde1ff527d8e89
akindofcode <akindofcode@users.noreply.github.com>
2018-03-23 17:18:23 +0100

    added zippers and unzippers

--- 007-basic-apps.sh
+++ 007-basic-apps.sh
@@ -3,3 +3,6 @@
 sudo pacman -S --noconfirm --needed vivaldi vivaldi-ffmpeg-codecs sublime-text
 
 sudo pacman -S --noconfirm --needed pcmanfm-gtk3 htop scrot ntfs-3g
+
+# installation of zippers and unzippers
+sudo pacman -S --noconfirm --needed unace unrar zip unzip sharutils uudeview arj cabextract file-roller
