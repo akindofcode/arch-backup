604a89a73d205a4ec11156195281a91292419051
akindofcode <akindofcode@users.noreply.github.com>
2018-03-23 17:29:41 +0100

    added fonts

--- 009-install-themes-fonts.sh
+++ 009-install-themes-fonts.sh
@@ -7,3 +7,6 @@ packer -S --noconfirm --noedit arc-gtk-theme
 # packer -S --noconfirm --noedit breeze-snow-cursor-theme	
 packer -S --noconfirm --noedit breeze-default-cursor-theme
 
+sudo pacman -S --noconfirm --needed noto-fonts noto-fonts-emoji noto-fonts-extra
+
+packer -S --noconfirm --noedit ttf-font-awesome-4
