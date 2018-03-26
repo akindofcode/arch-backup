edde751ad740fa6c78e7b131e3f12041884e0e14
akindofcode <akindofcode@users.noreply.github.com>
2018-03-26 10:19:56 +0200

    added openbox-arc-git

--- 009-install-themes-fonts.sh
+++ 009-install-themes-fonts.sh
@@ -2,7 +2,7 @@
 set -e
 
 #arc themes and icons
-packer -S --noconfirm --noedit arc-gtk-theme
+packer -S --noconfirm --noedit arc-gtk-theme openbox-arc-git
 
 # packer -S --noconfirm --noedit breeze-snow-cursor-theme	
 packer -S --noconfirm --noedit breeze-default-cursor-theme
