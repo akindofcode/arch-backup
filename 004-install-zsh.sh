1b4aaa239565f7bf1c265706791fc4d4631eeddc
akindofcode <akindofcode@users.noreply.github.com>
2018-03-26 20:56:35 +0200

    Update 004-install-zsh.sh

--- 004-install-zsh.sh
+++ 004-install-zsh.sh
@@ -2,19 +2,20 @@
 
 sudo pacman -S --noconfirm --needed zsh zsh-completions
 
-sudo pacman -S --noconfirm --needed zsh-theme-powerlevel9k
-
 sudo pacman -S --noconfirm --needed zsh-syntax-highlighting
 
 # install oh-my-zsh from github
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
 sudo pacman -S --noconfirm --needed powerline-fonts 
 
 #packer -S --noconfirm --noedit nerd-fonts-complete
 
-# To hange the theme, go to the hidden file ~/.zshrc
+# To change the theme, go to the hidden file ~/.zshrc
 # change this to ZSH_THEME="agnoster"
+# ZSH_THEME="powerlevel9k/powerlevel9k"
 
 #packer -S --noconfirm --noedit command-not-found
 
