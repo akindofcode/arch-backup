ba58efee803eb31a207b0773703857758207eda1
akindofcode <akindofcode@gmail.com>
2018-03-28 10:52:12 +0200

    added nitrogen config

--- 008-configure.sh
+++ 008-configure.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 #copy openbox configuration
-mkdir -p .config/openbox
-mkdir -p .config/obemenu-generator
+mkdir -p ~/.config/openbox
+mkdir -p ~/.config/obemenu-generator
 cp .config/openbox/* ~/.config/openbox
 cp .config/obmenu-generator/* ~/.config/obmenu-generator
+
+#copy nitrogen configuration
+mkdir -p ~/.config/nitrogen
+cp .config/nitrogen/* ~/.config/nitrogen
 
 #copy pipelines to /usr/bin
 sudo cp pipelines/al-* /usr/bin
