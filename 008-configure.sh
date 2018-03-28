15d048fac1b2545b95df0cb0f6b03771a856f285
akindofcode <akindofcode@gmail.com>
2018-03-28 10:59:18 +0200

    added vivaldi configuration

--- 008-configure.sh
+++ 008-configure.sh
@@ -18,4 +18,12 @@ cp .config/termite/* ~/.config/termite
 sudo cp pipelines/al-* /usr/bin
 sudo chown root:root /usr/bin/al-*
 
+#copy vivaldi conf - check for updates at https://github.com/Tiamarth/Arc-for-Vivaldi
+sudo cp .config/vivaldi/*.css /opt/vivaldi/resources/vivaldi/style
+sudo cp -R .config/vivaldi/arc /opt/vivaldi/resources/vivaldi/style
+sudo cp .config/vivaldi/browser.html /opt/vivaldi/resources/vivaldi/
+
+echo "!!! You need to modify the Vivaldi Theme by hand !!!"
+echo "Check values at https://github.com/Tiamarth/Arc-for-Vivaldi"
+
 echo "Reconfigure openbox and generate new menu"
