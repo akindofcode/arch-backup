ba86c021ccc26b91aebb2bc53f9a8364f48681e1
akindofcode <akindofcode@gmail.com>
2018-03-28 19:42:57 +0200

    added fixes

---
+++ 001-init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# configure git
+git config --global user.name "akindofcode"
+git config --global user.email "akindofcode@gmail.com"
+sudo git config --system core.editor "vim"
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=18000'
+git config --global push.default simple
+
+# blacklist nouveau module
+sudo cp init/blacklist.conf /etc/modprobe.d/
+sudo chown root:root /etc/modprobe.d/blacklist.conf
+
+# fix big fonts
+sudo nvidia-xconfig --no-use-edid-dpi
+
+# fix mount problems
+sudo cp init/00-mount-internal.rules /etc/polkit-1/rules.d/
+sudo chown root:root /etc/polkit-1/rules.d/00-mount-internal.rules
+
+echo "Please restart to apply changes"
