35cfe2fba4e3f31060007f64f942d71482001a17
akindofcode <akindofcode@users.noreply.github.com>
2018-04-03 19:56:12 +0200

    added nerd fonts

--- 007-install-themes-fonts.sh
+++ 007-install-themes-fonts.sh
@@ -13,3 +13,10 @@ packer -S --noconfirm --noedit ttf-font-awesome-4
 
 mkdir -p ~/.themes
 cp -R .themes ~/
+
+#install nerd fonts
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts
+curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+curl -fLo "Shure Tech Mono Nerd Font.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/ShareTechMono/complete/Shure%20Tech%20Mono%20Nerd%20Font%20Complete.ttf
+fc-cache -fv
