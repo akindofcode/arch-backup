6155b2aa28e4fbd613eb58ab5f80a2b3c10370bd
akindofcode <akindofcode@users.noreply.github.com>
2018-04-03 10:22:43 +0200

    added imagemagick

--- 005-basic-apps.sh
+++ 005-basic-apps.sh
@@ -2,7 +2,7 @@
 
 sudo pacman -S --noconfirm --needed vivaldi vivaldi-ffmpeg-codecs sublime-text termite
 
-sudo pacman -S --noconfirm --needed pcmanfm-gtk3 htop scrot ntfs-3g
+sudo pacman -S --noconfirm --needed pcmanfm-gtk3 htop scrot imagemagick
 
 # installation of zippers and unzippers
 sudo pacman -S --noconfirm --needed unace unrar zip unzip sharutils uudeview arj cabextract file-roller
