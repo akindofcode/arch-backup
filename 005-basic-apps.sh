40dce5f4db64fc0d62799385d14f24f205a1cf99
akindofcode <akindofcode@users.noreply.github.com>
2018-04-03 10:23:26 +0200

    Update 005-basic-apps.sh

--- 005-basic-apps.sh
+++ 005-basic-apps.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S --noconfirm --needed vivaldi vivaldi-ffmpeg-codecs sublime-text termite
-
-sudo pacman -S --noconfirm --needed pcmanfm-gtk3 htop scrot imagemagick
+sudo pacman -S --noconfirm --needed vivaldi vivaldi-ffmpeg-codecs sublime-text termite pcmanfm-gtk3 htop scrot imagemagick
 
 # installation of zippers and unzippers
 sudo pacman -S --noconfirm --needed unace unrar zip unzip sharutils uudeview arj cabextract file-roller
