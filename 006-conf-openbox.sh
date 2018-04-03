3823d58d4d5bc5b0b340bdd59c47442d25ac691d
akindofcode <akindofcode@users.noreply.github.com>
2018-04-03 11:58:21 +0200

    Update 006-conf-openbox.sh

--- 006-conf-openbox.sh
+++ 006-conf-openbox.sh
@@ -4,13 +4,7 @@ set -e
 # copy default openbox configuration to ~/.config
 cp -R /etc/xdg/openbox ~/.config
 
-sudo pacman -S --noconfirm --needed obconf
-
-sudo pacman -S --noconfirm --needed lxappearance-gtk3 lxappearance-obconf-gtk3 
-
-sudo pacman -S --noconfirm --needed neofetch
-
-sudo pacman -S --noconfirm --needed oblogout
+sudo pacman -S --noconfirm --needed obconf lxappearance-gtk3 lxappearance-obconf-gtk3 neofetch oblogout
 
 sudo pacman -S --noconfirm --needed tint2 dmenu feh nitrogen rofi xfce4-appfinder gmrun numlockx xfce4-notifyd volumeicon compton
 
@@ -18,9 +12,9 @@ packer -S --noconfirm --noedit obmenu3
 
 packer -S --noconfirm --noedit obmenu-generator
 
-#packer -S --noconfirm --noedit yad
-
 packer -S --noconfirm --noedit obkey
+
+#packer -S --noconfirm --noedit yad
 
 #packer -S --noconfirm --noedit i3lock-color-git
 
