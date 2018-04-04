b58badb3a7f82bfac3fb75bc6a94c594a5685c17
akindofcode <akindofcode@gmail.com>
2018-04-04 14:14:27 +0200

    fix sublime-text installation

--- 005-basic-apps.sh
+++ 005-basic-apps.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo pacman -S --noconfirm --needed vivaldi vivaldi-ffmpeg-codecs sublime-text termite pcmanfm-gtk3 htop scrot imagemagick
+sudo pacman -S --noconfirm --needed vivaldi vivaldi-ffmpeg-codecs termite pcmanfm-gtk3 htop scrot imagemagick
+
+sudo pacman -S --noconfirm --needed sublime-text/sublime-text
 
 # installation of zippers and unzippers
 sudo pacman -S --noconfirm --needed unace unrar zip unzip sharutils uudeview arj cabextract file-roller
