1c564e2398b9dc2bfb8208e7deeb794151f8e042
akindofcode <akindofcode@gmail.com>
2018-04-06 14:25:33 +0200

    music

---
+++ 009-music.sh
@@ -0,0 +1,9 @@
+ #!/bin/bash
+
+cp -r ~/.config ~/Projects/arch-install/
+cp ~/.Xresources ~/Projects/arch-install/
+cp ~/.zshrc ~/Projects/arch-install/
+cp -r ~/.toys ~/Projects/arch-install/
+cp -r ~/.bin ~/Projects/arch-install/
+
+echo "### dots backup done ###"
