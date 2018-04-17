b08ff4b0ae462da018740c715648e81522d7792a
akindofcode <akindofcode@gmail.com>
2018-04-17 09:53:31 +0200

    clean

--- 002-install-packer-and-repos.sh
+++ 002-install-packer-and-repos.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 set -e
 
+sudo pacman -S --noconfirm --needed grep sed bash curl pacman jshon expac wget
+
+[ -d /tmp/packer ] && rm -rf /tmp/packer
+
+mkdir /tmp/packer
+
+wget https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=packer
+
+mv PKGBUILD\?h\=packer /tmp/packer/PKGBUILD
+
+cd /tmp/packer
+
+makepkg -i /tmp/packer --noconfirm
+
+[ -d /tmp/packer ] && rm -rf /tmp/packer
+
+# Just checking if installation was successful
+if pacman -Qi packer &> /dev/null; then
+
+echo "### packer has been installed ####"
+
+else
+
+echo "!!! packer has NOT been installed !!!"
+
+fi
+
 sudo pacman -S --noconfirm --needed reflector
 
 sudo reflector -l 100 -f 50 --sort rate --threads 5 --verbose --save /tmp/mirrorlist.new && rankmirrors -n 0 /tmp/mirrorlist.new > /tmp/mirrorlist && sudo cp /tmp/mirrorlist /etc/pacman.d
