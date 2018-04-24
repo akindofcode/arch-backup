fd56145a77b756903537fa0436a23cc95930d2d3
akindofcode <akindofcode@gmail.com>
2018-04-24 12:41:13 +0200

    added mononoki font

--- 005-install-themes-fonts.sh
+++ 005-install-themes-fonts.sh
@@ -18,7 +18,7 @@ packer -S --noconfirm --noedit breeze-default-cursor-theme
 
 sudo pacman -S --noconfirm --needed noto-fonts
 
-packer -S --noconfirm --noedit ttf-font-awesome-4
+packer -S --noconfirm --noedit ttf-font-awesome-4 ttf-mononoki
 
 mkdir -p ~/.themes
 cp -r .themes/* ~/.themes/
